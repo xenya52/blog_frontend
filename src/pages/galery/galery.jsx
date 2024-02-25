fc35f6b3e43c56a47ba5f86021f648d940911da5
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-02-25 21:42:13 +0100

    desinged galery page, set margin top and bottom for footer and header

--- src/pages/galery/galery.jsx
+++ src/pages/galery/galery.jsx
@@ -7,7 +7,30 @@ function Galery() {
     return(
         <div>
             <Header />
-            <p>Galery</p>
+            <h1 className='galery-topic-headline'>Project Galery</h1>
+            <div className='galery-grid-container'>
+                <div className='galery-grid'>
+                    <img src='' className='galery-grid-img' />
+                </div>
+                <div className='galery-grid'>
+                    <h1 className='galery-grid-headline'>Lorem</h1>
+                    <p className='galery-grid-text'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className='galery-grid'>
+                    <img src='' className='galery-grid-img' />
+                </div>
+                <div className='galery-grid'>
+                    <h1 className='galery-grid-headline'>Lorem</h1>
+                    <p className='galery-grid-text'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className='galery-grid'>
+                    <img src='' className='galery-grid-img' />
+                </div>
+                <div className='galery-grid'>
+                    <h1 className='galery-grid-headline'>Lorem</h1>
+                    <p className='galery-grid-text'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
             <Footer />
         </div>
     )
