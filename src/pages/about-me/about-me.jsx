6d0eb05ece6232f7c8e773db8e43d7a04ec9cc87
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-03-24 09:33:18 +0100

    make a flipcard in my about-me page

--- src/pages/about-me/about-me.jsx
+++ src/pages/about-me/about-me.jsx
@@ -1,23 +1,37 @@
 import './about-me.css'
-import dumby from '../../assets/pictures/testBackground.png'
+import lucyPic from '../../assets/pictures/lucy.jpeg'
+import FlipCard from './flipcard/flipcard.jsx'
 
 function AboutMe() {
+
+    const aboutMeData =
+        {
+            id: 1,
+            img: lucyPic,
+            prename: "Lucy",
+            age: 20,
+            height: 180,
+            pronounce: "she/her",
+            skills: ["rust", "react", "git"],
+            hobbys: ["yoga", "programming", "shibari", "reading", "games"]
+        }
+
     return(
         <div>
             <h1 className='topic-h1-headline'>About me</h1>
             <div className='content-container'>
-                <div>
-                    <img  className='img-default'src={dumby}></img>
+                <div className='about-me-flip-container'>
+                    <FlipCard key={aboutMeData.id} {...aboutMeData} />
+                    {/* <img  className='img-default'src={lucyPic}></img> */}
                 </div>
-                <h3>Lorem ipsum</h3>
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-                <h3>Lorem ipsum</h3>
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-                <h3>Lorem ipsum</h3>
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-                <h3>Lorem ipsum</h3>
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-            </div>
+                    <div className='about-me-text-container'>
+                        <h3>Keep things simple</h3>
+                        <p>I'm not really long on this planet but I try every time to get a system into my toughts and actions. 
+                            Thats hard for me because earth is complicated. 
+                            And sometimse it's just easyer to keep things simple.</p>
+                        <h3>Software is my passion</h3>
+                    </div>
+                </div>
         </div>
     )
 }
---
+++ src/pages/about-me/flipcard/flipcard.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './flipcard.css'
+
+function Flipcard(props) {
+    const {id, img, prename, age, height, pronounce, skills, hobbys} = props
+    
+    const [isFlipped, setIsFlipped] = useState(false)
+    const toggleFlip = () => setIsFlipped(!isFlipped)
+
+    return (
+        <div 
+            className={`flip-card-container ${isFlipped ? "flipped" : ""}`}
+            onClick={toggleFlip}>
+            <div className='flip-card'>
+                {!isFlipped && (
+                    <div className='front-flip-card'>
+                        <img className='img-default' src={img} alt="Profile"/>
+                    </div>
+                )}
+                {isFlipped && (
+                    <div className='back-flip-card'>
+                        <ul>
+                            <li>Prename: {prename}</li>
+                            <li>Age: {age}</li>
+                            <li>Height: {height}cm</li>
+                            <li>Pronounce: {pronounce}</li>
+                            <li>Skills: 
+                                <ul>
+                                    {skills.map((skill, index) => <li key={index}>{skill}</li>)}
+                                </ul>
+                            </li>
+                            <li>Hobbys: 
+                                <ul>
+                                    {hobbys.map((hobby, index) => <li key={index}>{hobby}</li>)}
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Flipcard;
