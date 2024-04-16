d833a0a0875c38648e39c37fbe7a2617fc0f0d18
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-04-16 19:03:47 +0200

    todo post reworked - style todo posts - begin with a checked method to remove posts

--- src/pages/todos/todo-post.jsx
+++ src/pages/todos/todo-post.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import todoContent from '../../assets/todo-content.json';
 
+import './todo.css'
+
 function TodoPost() {
+  let [content, setContent] = useState(todoContent)
+
+  const handleChecked = (i) => {
+    setContent(content.slice(i))
+  }
+
   return (
     <div>
       {todoContent.map((content, index) => (
-        <div key={index}>
-          <h3>{content.title}</h3>
+        <div className='todo-post-container' key={index}>
+          <div>
+            <h3>
+              <input type='checkbox'/>
+              {content.title}
+            </h3>
+          </div>
           <p>{content.description}</p>
         </div>
       ))}
